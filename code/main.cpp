04f093df90c0bb3cc6d26d361434359da3ed5eca
Pacmega <bas.witters@hotmail.com>
2017-12-28 13:48:44 -0800

    Placeholder so it compiles

---
+++ code/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "TrafficLight.h"
+
+int main(int argc, char const *argv[])
+{
+	std::cout << "delete Sluice;" << std::endl;
+	std::cout << "Sluice == NULL;" << std::endl;
+	return 0;
+}
