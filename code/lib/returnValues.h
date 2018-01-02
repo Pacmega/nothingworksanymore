d8b7189553f418eddbd782c93a621f98dddf70c9
Pacmega <bas.witters@hotmail.com>
2018-01-02 15:38:19 -0800

    Fewer magic numbers, defined return values

---
+++ code/lib/returnValues.h
@@ -0,0 +1,16 @@
+#ifndef RETURNVALUES_H_
+#define RETURNVALUES_H_
+
+const int success = 0;
+const int noAckReceived = -1;
+const int incorrectWaterLevel = -2;
+const int incorrectDoorState = -3;
+const int invalidLightLocation = -4;
+const int invalidLightState = -5;
+const int motorDamaged = -6;
+const int interruptReceived = -7;
+const int invalidCall = -8;
+const int invalidWaterLevel = -9;
+const int workInProgress = 420;
+
+#endif
